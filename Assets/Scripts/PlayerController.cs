3f0ec78a19789cbbfa367e913a159c52f9016c40
Sondre Agledahl <sondre.milch@gmail.com>
2018-06-23 08:56:01 +0100

    Added placeholder level w/ horizontally moving player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // --------------------------------------------------------------
+
+    [SerializeField] private float m_WalkSpeed;
+
+    // --------------------------------------------------------------
+
+    private CharacterController m_CharController;
+
+    private Vector3 m_MovementDirection;
+
+    private Vector3 m_MovementOffset;
+
+    private float m_VerticalSpeed;
+
+    // --------------------------------------------------------------
+
+    private void Awake()
+    {
+        m_CharController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        UpdateMovementDirection();
+
+        m_MovementOffset = (m_MovementDirection * m_WalkSpeed + new Vector3(0, m_VerticalSpeed, 0)) * Time.deltaTime;
+
+        m_CharController.Move(m_MovementOffset);
+
+    }
+
+    private void UpdateMovementDirection()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        m_MovementDirection = new Vector3(horizontalInput, 0, verticalInput);
+    }
+}
